d35b3839f393904b8ff6ede30fc7bd3b2862bfe7
Владимир Рязанов <ryazanov745@gmail.com>
2022-08-07 16:49:33 +0300

    update v0.8.8

--- backend/api/management/commands/load_ingrs.py
+++ backend/api/management/commands/load_ingrs.py
@@ -3,20 +3,24 @@ import csv
 from django.conf import settings
 from django.core.management import BaseCommand
 
+
 from api.models import Ingredient
 
 
 class Command(BaseCommand):
-    help = 'Загрузка из csv файла'
+    help = 'Add csv files to Django Models.'
 
     def handle(self, *args, **kwargs):
         data_path = settings.BASE_DIR
         with open(
-            f'{data_path}/data/ingredients.csv',
-            'r',
+            f'{ data_path }/data/ingredients.csv',
+            newline='',
             encoding='utf-8'
         ) as file:
-            reader = csv.DictReader(file)
-            Ingredient.objects.bulk_create(
-                Ingredient(**data) for data in reader)
-        self.stdout.write(self.style.SUCCESS('Все ингридиенты загружены!'))
+            spamreader = csv.reader(file, delimiter=',')
+            for row in spamreader:
+                Ingredient.objects.update_or_create(
+                        name=row[0],
+                        units=row[1]
+                    )
+            self.stdout.write(self.style.SUCCESS('Все ингредиенты загружены.'))
