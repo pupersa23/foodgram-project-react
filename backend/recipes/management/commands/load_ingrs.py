29cdddab480a6c388c7b2f7a6df861cd9a0deec8
Владимир Рязанов <ryazanov745@gmail.com>
2022-08-11 15:00:32 +0300

    update v1.3.2

--- backend/recipes/management/commands/load_ingrs.py
+++ backend/recipes/management/commands/load_ingrs.py
@@ -1,22 +1,22 @@
-import csv
-
-from django.conf import settings
-from django.core.management import BaseCommand
-
-from recipes.models import Ingredient
+import json
 
 
-class Command(BaseCommand):
-    help = 'Загрузка из csv файла'
+def add_parametrs(ingredient, id):
+    return {
+        "model": "recipes.ingredient",
+        "pk": id,
+        "fields": ingredient
+    }
 
-    def handle(self, *args, **kwargs):
-        data_path = settings.BASE_DIR
-        with open(
-            f'{data_path}/data/ingredients.csv',
-            'r',
-            encoding='utf-8'
-        ) as file:
-            reader = csv.DictReader(file)
-            Ingredient.objects.bulk_create(
-                Ingredient(**data) for data in reader)
-        self.stdout.write(self.style.SUCCESS('Все ингридиенты загружены!'))
+
+def create_fixtures():
+    with open('ingredients.json', 'r') as f:
+        ingredients = json.loads(f.read())
+        for i in range(len(ingredients)):
+            ingredients[i] = add_parametrs(ingredients[i], i + 1)
+    with open('fixtures.json', 'w') as f:
+        f.write(json.dumps(ingredients))
+
+
+if __name__ == '__main__':
+    create_fixtures()
